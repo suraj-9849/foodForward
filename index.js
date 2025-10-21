f3b3f6461d0652f9c7d94d8effe6e74c41c53509
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2025-10-21 09:31:06 +0530

    fixed

--- index.js
+++ index.js
@@ -48,4 +48,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+}
+);
+
 module.exports = app;
